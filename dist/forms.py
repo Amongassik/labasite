24acbac48af517a873521b60a3c544f2ca623339
Amongassik <118072713+Amongassik@users.noreply.github.com>
2026-04-01 17:43:20 +0300

    form login

--- dist/forms.py
+++ dist/forms.py
@@ -1,7 +1,35 @@
 from django import forms
+from django.contrib.auth.hashers import check_password
 from .models import Emploees,Logins
 
-class LoginForm(forms.ModelForm):
-    class Meta:
-        model = Logins
-        fields = '__all__'
+class LoginForm(forms.Form):
+    login = forms.CharField(
+        label='Логин',
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите логин'
+        })
+    )
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите пароль'
+        })
+    )
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        login = cleaned_data.get('login')
+        password = cleaned_data.get('password')
+
+        if login and password:
+            try:
+                user = Logins.objects.get(login=login)
+                if not user.check_password(password):
+                    raise forms.ValidationError("Неверный пароль")
+                cleaned_data['user'] = user
+            except Logins.DoesNotExist:
+                raise forms.ValidationError('Пользователь с таким логином не найден')
+        return cleaned_data
